c77b66d9eaf207726476c4212f8d4cc137a8ebbe
Reda-Yh <redayahyapro@gmail.com>
2025-01-20 21:40:52 +0100

    add images and top-button and translating to english

--- travel_v1.31/js/main.js
+++ travel_v1.31/js/main.js
@@ -59,3 +59,31 @@ if (window.scrollY === 0) {
         link.style.color = ''; // Couleur initiale
     });
 }
+
+// Fonction pour remonter en haut de la page
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Ajouter le bouton flottant en HTML
+window.addEventListener('DOMContentLoaded', () => {
+    const scrollButton = document.createElement('button');
+    scrollButton.classList.add('scroll-to-top');
+    scrollButton.innerHTML = '<i class="bi bi-arrow-up"></i>';
+    document.body.appendChild(scrollButton);
+
+    // Ecouteur d'événements pour le clic sur le bouton
+    scrollButton.addEventListener('click', scrollToTop);
+
+    // Affichage du bouton au début du scroll
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            scrollButton.style.display = 'flex';
+        } else {
+            scrollButton.style.display = 'none';
+        }
+    });
+});
